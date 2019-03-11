3bc458b3e9e2c7d52596b512133d5db6ef5b7c24
anthony <anthony@gmail.com>
2019-03-11 09:26:11 +0100

    add Carpeta Recursos

---
+++ AplicacionChat/public_html/recursos/funcionesJS/js_index.js
@@ -0,0 +1,13 @@
+/****************EVENTOS********************/
+//ESCUCHADOR DE EVENTOS//
+document.addEventListener("readystatechange", cargarEventos, false);
+function cargarEventos(evento) {
+    if (document.readyState === "complete") {
+        
+
+
+    }
+
+}
+
+
